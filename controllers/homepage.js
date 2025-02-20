8b994d0189212c6a073152a67a366201f5b1e08f
manellylopez <manellylopezqqq@gmail.com>
2025-02-20 12:45:38 -0500

    omit users details in get all post json answer

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -2,17 +2,15 @@ const db_posts = require("../prisma_queries/posts");
 
 // GET /
 async function get(req, res) {
-  // MODIFICAR para mostrar solo los post con la opcion de publicados
   const allPosts = await db_posts.getAllPosts();
 
   res.status(200).json({
     title: "BLOG | HOMEPAGE",
     message: "Welcome to the API ",
     allPosts,
-    user: req.user,
   });
 }
 
 module.exports = {
-    get,
-}
+  get,
+};
