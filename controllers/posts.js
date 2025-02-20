bef122ad275ca8d36b78582be78271f33d3c9d7b
manellylopez <manellylopezqqq@gmail.com>
2025-02-20 12:47:50 -0500

    /posts/:authorid GET method route and controllers

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,35 +1,53 @@
 const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
-const { v4 : uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const myObject = {};
 require("dotenv").config({ processEnv: myObject });
 const secret_key = process.env.SECRET_KEY || myObject.SECRET_KEY;
 
 const db_posts = require("../prisma_queries/posts");
-
+const db_users = require("../prisma_queries/users");
 
 async function get(req, res) {
   const allPosts = await db_posts.getAllPosts();
   res.status(200).json({
     allPosts,
-    user: req.user,
   });
 }
 
-async function getNew(req, res) {
-    jwt.verify(req.token, secret_key, (err, authData) => {
-        if (err) {
-          console.log(err);
-          res.sendStatus(403);
-        } else {
-          return res.status(200).json({
-            title: "BLOG | CREATE NEW BLOG",
-            user: req.user,
-            authData,
-          });
-        }
+async function getByAuthor(req, res) {
+  const { authorid } = req.params;
+  const authorExists = await db_users.authorExists(authorid);
+  switch (authorExists) {
+    case true:
+      const postsByAuthor = await db_posts.getByAuthor(authorid);
+      res.status(200).json({
+        postsByAuthor,
       });
+      break;
+    case false:
+      res.status(400).json({
+        text: "this author does not exist",
+      });
+      break;
+  }
+}
+
+// Following routes require authentication
+async function getNew(req, res) {
+  jwt.verify(req.token, secret_key, (err, authData) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(403);
+    } else {
+      return res.status(200).json({
+        title: "BLOG | CREATE NEW BLOG",
+        user: req.user,
+        authData,
+      });
+    }
+  });
 }
 
 // validate new blog content
@@ -63,13 +81,13 @@ const postNew = [
       });
     }
     const id = uuidv4();
-    await db_posts.createNewPost(req,res,id,authData);
-
+    await db_posts.createNewPost(req, res, id, authData);
   },
 ];
 
 module.exports = {
   get,
+  getByAuthor,
   getNew,
   postNew,
 };
--- routes/post.js
+++ routes/post.js
@@ -6,12 +6,13 @@ const router = Router();
 
 router.get("/", postsController.get);
 
-router.get("/:authorid");
+router.get("/:authorid", postsController.getByAuthor);
 
 router.get("/:authorid/:postid");
 
 router.get("/:authorid/:postid/comments");
 
+// Following routes require authentication
 router.get("/workspace", isAuth, roleAuthor, verifyToken);
 
 router.get("/new", isAuth, roleAuthor, verifyToken, postsController.getNew);
