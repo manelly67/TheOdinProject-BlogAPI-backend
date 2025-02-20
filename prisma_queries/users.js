83583a71cb80d4cf8138253d18747585af4b12c9
manellylopez <manellylopezqqq@gmail.com>
2025-02-20 11:37:58 -0500

    query function authorExists

--- prisma_queries/users.js
+++ prisma_queries/users.js
@@ -4,10 +4,8 @@ const prisma = new PrismaClient();
 const passwordRequirements =
   "Password must contain at least one number, one uppercase and lowercase letter, one special character, and at least 8 or more characters";
 
-
 async function createUser(req, res, hashedPassword) {
-    
-    await prisma.user
+  await prisma.user
     .create({
       data: {
         email: `${req.body.email}`,
@@ -17,57 +15,69 @@ async function createUser(req, res, hashedPassword) {
       },
     })
     .then(async () => {
-        const userCreated = await getUserFromUsername(req.body.username);
-        await prisma.$disconnect();
-        res.json({
-          text:'user created login in your account', 
-          user:userCreated,
-        });
+      const userCreated = await getUserFromUsername(req.body.username);
+      await prisma.$disconnect();
+      res.json({
+        text: "user created login in your account",
+        user: userCreated,
+      });
     })
     .catch(async (err) => {
-        if (err.code === "P2002") {
-            const errFields = [
-              { msg: `fields [ ${err.meta.target}] is already taken.` },
-            ];
-            return res.status(400).json({
-                title: "BLOG | New User",
-                user: req.user,
-                passwordRequirements: passwordRequirements,
-                errors: errFields,
-              });
-          } else {
-            await prisma.$disconnect();
-            process.exit(1);
-          }
-    })
-  };
-
-  const getUserFromUsername = async (username) => {
-    return await prisma.user
-      .findUnique({
-        where: { username: username },
-        include: {
-          posts: true,
-          comments: true,
-        },
-      });
-  };
-  
-  const getUserFromId = async(id) => {
-    return await prisma.user
-    .findUnique({
-      where: {
-        id: Number(id),
-      },
-      include: {
-        posts: true,
-        comments: true,
-      },
+      if (err.code === "P2002") {
+        const errFields = [
+          { msg: `fields [ ${err.meta.target}] is already taken.` },
+        ];
+        return res.status(400).json({
+          title: "BLOG | New User",
+          user: req.user,
+          passwordRequirements: passwordRequirements,
+          errors: errFields,
+        });
+      } else {
+        await prisma.$disconnect();
+        process.exit(1);
+      }
     });
-  };
+}
+
+const getUserFromUsername = async (username) => {
+  return await prisma.user.findUnique({
+    where: { username: username },
+    include: {
+      posts: true,
+      comments: true,
+    },
+  });
+};
+
+const getUserFromId = async (id) => {
+  return await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      posts: true,
+      comments: true,
+    },
+  });
+};
+
+async function authorExists(id) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+  if (user !== null && user.role === "AUTHOR") {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = {
-    createUser,
-    getUserFromUsername,
-    getUserFromId,
+  createUser,
+  getUserFromUsername,
+  getUserFromId,
+  authorExists,
 };
