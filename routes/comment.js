10a44871476c2d50c3a42cfd91cc0446cc3f359f
manellylopez <manellylopezqqq@gmail.com>
2025-02-21 15:45:49 -0500

    adding routes /comments

--- routes/comment.js
+++ routes/comment.js
@@ -1,5 +1,18 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
+const { isAuth, verifyToken } = require("./middlewares");
 
+router.get("/:userid");
+
+router.get("/:userid/:postid");
+
+router.get("/:userid/:postid/new",isAuth, verifyToken);
+router.post("/:userid/:postid/new",isAuth, verifyToken);
+
+router.get("/:userid/:postid/:commentid");
+
+router.put("/:userid/:postid/:commentid",isAuth, verifyToken);
+
+router.delete("/:userid/:postid/:commentid",isAuth, verifyToken);
 
 module.exports = router;
