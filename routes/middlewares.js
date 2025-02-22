9f6f748879962715ba6aa7a0a533d5bc65cec1f8
manellylopez <manellylopezqqq@gmail.com>
2025-02-22 11:50:10 -0500

    add middleware author id must be a number

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -31,6 +31,20 @@ module.exports.useridIsNumber = (req, res, next) => {
   }
 };
 
+module.exports.authoridIsNumber = (req, res, next) => {
+  let { authorid } = req.params;
+  authorid = Number(authorid);
+
+  if (Number.isNaN(authorid)) {
+    // check is a value is equal NaN
+    res.status(400).json({
+      text: "the author id must be a number",
+    });
+  } else {
+    next();
+  }
+};
+
 module.exports.clearMessages = (req, res, next) => {
   req.session.messages = null;
   next();
