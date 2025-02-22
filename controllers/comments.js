383f45551e8d8df67e374db94047d244c096ed53
manellylopez <manellylopezqqq@gmail.com>
2025-02-22 11:31:35 -0500

    method GET /comments/:userid/:postid route and controllers

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,34 +1,60 @@
 const db_users = require("../prisma_queries/users");
+const db_posts = require("../prisma_queries/posts");
 const db_comments = require("../prisma_queries/comments");
 
 async function getByUser(req, res) {
-  let { userid } = req.params;
-  userid = Number(userid);
-
-  if (Number.isNaN(userid)) {
-    // check is a value is equal NaN
-    res.status(400).json({
-      text: "the user id must be a number",
-    });
-  } else {
-    const userExists = await db_users.userExists(userid);
-    switch (userExists) {
-      case true: {
-        const commentsByUser = await db_comments.getByUser(userid);
+  const { userid } = req.params;
+  const userExists = await db_users.userExists(userid);
+  switch (userExists) {
+    case false:
+      res.status(400).json({
+        text: "this user does not exist",
+      });
+      break;
+    case true:
+      {
+        const comments = await db_comments.getByUser(userid);
         res.status(200).json({
-          commentsByUser,
+          commentsByUser: comments,
+          userid: userid,
         });
-        break;
       }
-      case false:
-        res.status(400).json({
-          text: "this user does not exist",
-        });
-        break;
-    }
+      break;
+  }
+}
+
+async function getByUserAndPost(req, res) {
+  const { userid, postid } = req.params;
+  const userExists = await db_users.userExists(userid);
+  const postExists = await db_posts.postExists(postid);
+  switch (userExists) {
+    case false:
+      res.status(400).json({
+        text: "this user does not exist",
+      });
+      break;
+    case true:
+      switch (postExists) {
+        case false:
+          res.status(400).json({
+            text: "this post does not exist",
+          });
+          break;
+        case true:
+          {
+            const comments = await db_comments.getByUserAndPost(userid, postid);
+            res.status(200).json({
+              commentsByUserAndPost: comments,
+              userid: userid,
+              postid: postid,
+            });
+          }
+          break;
+      }
   }
 }
 
 module.exports = {
   getByUser,
+  getByUserAndPost,
 };
--- routes/comment.js
+++ routes/comment.js
@@ -1,19 +1,21 @@
 const { Router } = require("express");
 const router = Router();
-const { isAuth, verifyToken } = require("./middlewares");
+const { isAuth, verifyToken, useridIsNumber } = require("./middlewares");
 const commentController = require("../controllers/comments");
 
-router.get("/:userid",commentController.getByUser);
+router.get("/:postid");
 
-router.get("/:userid/:postid");
+router.get("/:userid", useridIsNumber, commentController.getByUser);
 
-router.get("/:userid/:postid/new",isAuth, verifyToken);
-router.post("/:userid/:postid/new",isAuth, verifyToken);
+router.get("/:userid/:postid", useridIsNumber, commentController.getByUserAndPost);
+
+router.get("/:userid/:postid/new", isAuth, verifyToken);
+router.post("/:userid/:postid/new", isAuth, verifyToken);
 
 router.get("/:userid/:postid/:commentid");
 
-router.put("/:userid/:postid/:commentid",isAuth, verifyToken);
+router.put("/:userid/:postid/:commentid", isAuth, verifyToken);
 
-router.delete("/:userid/:postid/:commentid",isAuth, verifyToken);
+router.delete("/:userid/:postid/:commentid", isAuth, verifyToken);
 
 module.exports = router;
